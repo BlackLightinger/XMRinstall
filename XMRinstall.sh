7250cdecfaea421b266fe9b448ade862a8b237fb
BlackLightinger <104649015+BlackLightinger@users.noreply.github.com>
2022-11-30 01:17:22 +0300

    Update XMRinstall.sh

--- XMRinstall.sh
+++ XMRinstall.sh
@@ -21,16 +21,37 @@ EOF
 }
 
 createservice(){
-cat <<EOF > /etc/systemd/system/$1.service
+cat <<EOF > /etc/systemd/system/$1service.service
 [Unit]
 Description=Run a script on server startup
 
 [Service]
 Type=oneshot
-ExecStart=/root/$1.sh
+ExecStart=/root/$1script.sh
 
 [Install]
 WantedBy=multi-user.target
+
+EOF
+}
+
+create_SettingXMRMiner(){
+cat <<EOF > /root/SettingXMRMiner.sh
+#!/bin/bash
+
+rm bufscript.sh
+sudo systemctl disable bufservice
+rm /etc/systemd/system/bufservice.service
+
+cd root
+sudo apt-get install -y git build-essential cmake automake libtool autoconf
+git clone https://github.com/xmrig/xmrig.git
+mkdir xmrig/build && cd xmrig/scripts
+./build_deps.sh && cd ../build
+cmake .. -DXMRIG_DEPS=scripts/deps
+make -j$(nproc)
+mv config.json xmrig/build/
+/root/xmrig/build/./xmrig
 
 EOF
 }
@@ -41,19 +62,14 @@ sudo apt -y upgrade
 sudo apt-get -y update
 sudo apt autoremove
 createconfig
+create_SettingXMRMiner
 
 
-cat <<EOF > /root/buf.sh
+cat <<EOF > /root/bufscript.sh
 #!/bin/bash
 
-cd root
-sudo apt-get install -y git build-essential cmake automake libtool autoconf
-git clone https://github.com/xmrig/xmrig.git
-mkdir xmrig/build && cd xmrig/scripts
-./build_deps.sh && cd ../build
-cmake .. -DXMRIG_DEPS=scripts/deps
-make -j$(nproc)
-mv config.json xmrig/build/
+chmod +x SettingXMRMiner.sh
+./SettingXMRMiner.sh
 
 EOF
 
